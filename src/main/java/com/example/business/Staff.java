fb345bd68b31415e7eadee890a4098f0d2f2e64f
Krishna Patel <krishna.patel0621@gmail.com>
2023-10-23 19:28:00 -0400

    downloading and writing xml functions

--- src/main/java/com/example/business/Staff.java
+++ src/main/java/com/example/business/Staff.java
@@ -1,14 +1,20 @@
 package com.example.business;
 
+import java.util.HashMap;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+
 public class Staff extends User{
 
     public Staff(String username, String password){
         super(username, password);
     }
 
-    public void createProduct(String sku, String name){
-        Product temp = new Product(name, "", "", sku, 0, "");
-        globalVariables.allProductsSku.put(sku, temp);
+    public void createProduct(HashMap<String, Product> map, String sku, String name){
+        Product temp = new Product(name, "","", "", sku, 0, "");
+        map.put(sku, temp);
     }
 
     /**
@@ -21,12 +27,15 @@ public class Staff extends User{
      * @param price
      * @param imgSrc
      */
-    public void updateProduct(Product updatedProd,String name, String description, String URL, String SKU, double price, String imgSrc){
+    public void updateProduct(Product updatedProd,String name, String description, String vendor, String URL, String SKU, double price, String imgSrc){
         if(!name.isBlank()){
             updatedProd.setName(name);
         }
         if(!description.isBlank()){
             updatedProd.setDescription(description);
+        }
+        if(!vendor.isBlank()){
+            updatedProd.setVendor(vendor);
         }
         if(!URL.isBlank()){
             updatedProd.setURL(URL);
@@ -41,5 +50,49 @@ public class Staff extends User{
             updatedProd.setImgSrc(imgSrc);
         }
     }
+
+    public File downloadProductCatalog(HashMap<String, Product> allProductsSku, String filePath){
+
+        writeProductsToXML(allProductsSku, filePath);
+        return new File(filePath);
+    }
+
+    public static void writeProductsToXML(HashMap<String, Product> productMap, String filePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            writer.newLine();
+            writer.write("<products>");
+            writer.newLine();
+
+            for (String productId : productMap.keySet()) {
+                Product product = productMap.get(productId);
+
+                writer.write("  <product>");
+                writer.newLine();
+                writer.write("    <name>" + product.getName() + "</name>");
+                writer.newLine();
+                writer.write("    <description>" + product.getDescription() + "</description>");
+                writer.newLine();
+                writer.write("    <vendor>" + product.getVendor() + "</vendor>");
+                writer.newLine();
+                writer.write("    <slug>" + product.getURL() + "</slug>");
+                writer.newLine();
+                writer.write("    <sku>" + product.getSKU() + "</sku>");
+                writer.newLine();
+                writer.write("    <Price>" + product.getPrice() + "</Price>");
+                writer.newLine();
+                writer.write("    <img>" + product.getImgSrc() + "</img>");
+                writer.newLine();
+                writer.write("  </product>");
+                writer.newLine();
+            }
+
+            writer.write("</products>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
--- src/main/java/com/example/business/storefrontFacade.java
+++ src/main/java/com/example/business/storefrontFacade.java
@@ -1,20 +1,25 @@
 package com.example.business;
 
 import java.util.HashMap;
+import java.io.File;
 
 public class storefrontFacade {
     public static HashMap<String,Product> allProductsSku;
+
+    final String filePathProducts = "C:\\Users\\Krish\\Documents\\School\\Concordia\\Projects\\PenCo\\src\\main\\resources\\products.xml";
+    final String getFilePathUsers = "C:\\Users\\Krish\\Documents\\School\\Concordia\\Projects\\PenCo\\src\\main\\resources\\users.xml";
+
     static HashMap<String, User> allUsers;
     static User currentUser;
 
     public void createProduct(Staff staff, String sku, String name) {
         // You may add authorization checks here to ensure staff privileges.
-        staff.createProduct(sku, name);
+        staff.createProduct(allProductsSku, sku, name);
     }
-    public void updateProduct(Product updatedProd, String name, String description, String URL, String SKU, double price, String imgSrc){
+    public void updateProduct(Product updatedProd, String name, String description, String vendor, String URL, String SKU, double price, String imgSrc){
         if (currentUser instanceof Staff) {
             Staff staffUser = (Staff) currentUser;
-            staffUser.updateProduct(updatedProd, name, description, URL, SKU, price, imgSrc);
+            staffUser.updateProduct(updatedProd, name, description, vendor, URL, SKU, price, imgSrc);
         } else {
             // Handle the case where currentUser is not a Staff
             // You could throw an exception or handle it in some other way
@@ -32,17 +37,43 @@ public class storefrontFacade {
     }
 
     public Cart getCart() {
-        if (currentUser instanceof Customer) {
-            Customer customer = (Customer) currentUser;
-            return customer.getCart();
-        } else {
-            // Handle the case where currentUser is not a Staff
-            // You could throw an exception or handle it in some other way
+        if (currentUser instanceof Staff) {
             System.out.println("The user is a staff and does not have a cart");
             return null;
+
+        } else {
+            Customer customer = (Customer) currentUser;
+            return customer.getCart();
         }
     }
-    
+
+    public void addProductToCart(String sku){
+        Product productToAdd = allProductsSku.get(sku);
+        if(currentUser instanceof Customer){
+            Customer customer = (Customer) currentUser;
+            customer.addProductToCart(productToAdd);
+        }
+    }
+
+    public void removeProductFromCart(String sku){
+        Product productToRemove = allProductsSku.get(sku);
+        if(currentUser instanceof Customer){
+            Customer customer = (Customer) currentUser;
+            customer.removeProductFromCart(productToRemove);
+        }
+    }
+
+    public File downloadProductCatalogue(){
+        if(currentUser instanceof Staff){
+            Staff staff = (Staff) currentUser;
+            staff.downloadProductCatalog(allProductsSku, filePathProducts);
+        }else{
+            System.out.println("Not a staff, cannot download catalogue.");
+            return null;
+        }
+            return null;
+    }
+
 }
 
 
