98e1c8f6603d25efb135a4733fa9777ea47f859d
briannamalpar <briemalpartida@gmail.com>
2023-11-08 22:00:46 -0500

    added servlet for getOrder() & getOrders()

--- src/main/java/com/example/penco/OrderServlet.java
+++ src/main/java/com/example/penco/OrderServlet.java
@@ -1,4 +1,30 @@
 package com.example.penco;
 
-public class OrderServlet {
+import com.example.business.*;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name="viewOrder", value="/order")
+public class OrderServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ServletContext servletContext = getServletContext();
+        User user = (User) servletContext.getAttribute("User");
+
+
+
+        // Forward to your JSP page for displaying order details
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/vieworder.jsp");
+        dispatcher.forward(request, response);
+    }
 }
--- src/main/java/com/example/penco/ViewOrdersServlet.java
+++ src/main/java/com/example/penco/ViewOrdersServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,20 +18,25 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet(name="viewOrders", value="/orders")
 public class ViewOrdersServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Order> orders = DatabaseHelper.getOrdersFromDatabase();
-        request.setAttribute("orders", orders);
 
         ServletContext servletContext = getServletContext();
         User user = (User) servletContext.getAttribute("User");
 
         storefrontFacade facade = new storefrontFacade();
+        if(user instanceof Customer){
+            List<Integer> orders = facade.getOrders(user);
+            // Set the 'orders' attribute so we can use in jsp
+            request.setAttribute("orders", orders);
+        }
 
-        List<Integer> order_ids = facade.getOrders(user);
+        if(user instanceof Staff){
+            List<Integer> orders = facade.getOrders(user);
+            // Set the 'orders' attribute so we can use in jsp
+            request.setAttribute("orders", orders);
+        }
 
-        request.setAttribute("order_ids", order_ids);
-
-
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/vieworders.jsp");
+        // Forward to your JSP page for displaying order details
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/viewOrders.jsp");
         dispatcher.forward(request, response);
     }
 }
